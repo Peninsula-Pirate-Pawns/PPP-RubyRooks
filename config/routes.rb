f6e55b34b438517aa1c0a30c05a0fe9589cb9003
Shane Quick <shanesfast@gmail.com>
2020-04-03 17:00:40 -0500

    Create user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   root 'games#index'
 
