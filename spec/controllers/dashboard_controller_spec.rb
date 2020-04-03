c2524231fde927f6ad705427c12d33d38edfb066
Shane Quick <shanesfast@gmail.com>
2020-04-03 17:02:58 -0500

    update dashboard controller spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe DashboardController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
+      user = create(:user)
+      sign_in user
+
       get :index
       expect(response).to have_http_status(:success)
     end
